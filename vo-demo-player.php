bd4fb3268cd15a2a87a4001f3355b69df3a5330b
omarkasem <omar.kasem207@gmail.com>
2024-10-15 04:17:20 +0300

    fix firefox

--- vo-demo-player.php
+++ vo-demo-player.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: VO Demo Player
  * Description: Customizable audio player with download option.
- * Version:     1.0.7
+ * Version:     1.0.8
  * Author:      Voice Acting 101 
  * Author URI: https://voiceacting101.com/demo-player/
  * License:     GPL v3
@@ -15,7 +15,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 
-define( 'OK_CAPS_VERSION', '1.0.7' );
+define( 'OK_CAPS_VERSION', '1.0.8' );
 define( 'OK_CAPS_NAME', 'vo-demo-player' );
 define( 'OK_CAPS_URL', plugin_dir_url( __FILE__ ) );
 define( 'OK_CAPS_PATH', plugin_dir_path( __FILE__ ) );
